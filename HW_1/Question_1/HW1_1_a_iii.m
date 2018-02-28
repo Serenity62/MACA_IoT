39deda87f0e566a90235aecfe5a9caa8f6b754cc
Steven J. Roote <sjr5444@psu.edu>
2018-02-28 16:51:50 -0500

    Add files via upload

--- HW_1/Question_1/HW1_1_a_iii.m
+++ HW_1/Question_1/HW1_1_a_iii.m
@@ -1,32 +1,38 @@
-%%HW1.1.a.iii.
+%% HW1.1.a.iii.
 clc;
 clear;
 close all;
 
-tic;
-%Import images from HW1 folder and convert to grayscale
+% Import images from HW1 folder and convert to grayscale
 image1 = rgb2gray(imread('dog.bmp'));
 image2 = rgb2gray(imread('motorcycle.bmp'));
 image3 = rgb2gray(imread('submarine.bmp'));
 
-%Take two-dimensional discrete Fourier Transform
-%Purpose: To put the imported images in the frequency domain...
-%so that we can seperate the phase from the domain 
+% Take two-dimensional discrete Fourier Transform
+% Purpose: To put the imported images in the frequency domain...
+% so that we can seperate the phase from the domain 
 f_im1 = fft2(image1);
 f_im2 = fft2(image2);
 f_im3 = fft2(image3);
 
-%This takes the magnitudes from the frequency of the images
+% This takes the magnitudes from the frequency of the images
 fmag_im1 = abs(f_im1);
 fmag_im2 = abs(f_im2);
 fmag_im3 = abs(f_im3);
 
-%This takes the inverse Fourier Transform and then shifts
-mag_im1 = fftshift(ifft2(fmag_im1));
-mag_im2 = fftshift(ifft2(fmag_im2));
-mag_im3 = fftshift(ifft2(fmag_im3));
-toc;
-%Shows results of magnitude of each image
-% figure; imshow(uint8(mag_im1)); title('Magnitude of Dog');
-% figure; imshow(uint8(mag_im2)); title('Magnitude of Motorcycle');
-% figure; imshow(uint8(mag_im3)); title('Magnitude of Submarine');
+% This takes the inverse Fourier Transform
+mag_im1 = ifft2(fmag_im1);
+mag_im2 = ifft2(fmag_im2);
+mag_im3 = ifft2(fmag_im3);
+
+% This Shifts the result
+mag_im1_s = fftshift(mag_im1);
+mag_im2_s = fftshift(mag_im2);
+mag_im3_s = fftshift(mag_im2);
+
+% Shows results of magnitude of each image
+figure; imshow(uint8(mag_im1_s)); title('Magnitude of Dog');
+figure; imshow(uint8(mag_im2_s)); title('Magnitude of Motorcycle');
+figure; imshow(uint8(mag_im3_s)); title('Magnitude of Submarine');
+
+
