51b47d5487f9675ae3c93e367c2e7e2c5216cf9b
Steven J. Roote <sjr5444@psu.edu>
2018-04-09 11:18:36 -0400

    Main -Skeleton Code

--- Project/main.m
+++ Project/main.m
@@ -1,3 +1,16 @@
 
+% Author(s): Aaron Barraclough, Michael Ferry, Steven Roote, Corey Zalewski
+
+%% Preprocessing (Image Processing and Feature Extraction)
+
+% Recieve New Frame From Webcam
+% INSERT CODE HERE
+
+% Non-linear Filter for Noise Reduction
+%frameFilter = INSERT CODE HERE
+
+% Gaussian Mixture Model for clustering image
+%frameGMM = INSERT CODE HERE
+
 % Use Canny Edge Detection Algorithm to generate binary file with edges
 frameCannyEdge = edge(frameGMM,'Canny');
