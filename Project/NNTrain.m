59a4132aba74b5acd085a52d3145dde77355f884
Serenity62 <cuz126@psu.edu>
2018-04-11 12:38:19 -0400

    Added in live feed and created training code

---
+++ Project/NNTrain.m
@@ -0,0 +1,65 @@
+
+
+%% Import Training data
+% load('train.mat');
+for i = 1 : 1400
+    frame = reshape(csv(i,:), [960, 1280]);
+    fr = imresize(frame, [480,640]);
+    tr(i, :) = reshape(fr, [1,480*640]);
+end
+%% Setup NN
+n = size(tr, 1);                    % number of samples in the dataset
+targets  = label; 
+targets(targets == 0) = 10;         % use '7' to present '0'
+targetsd = dummyvar(targets);       % convert label into a dummy variable
+
+% No need for the first column in the (tr) set any longer
+inputs = tr;               % the rest of columns are predictors
+
+inputs = inputs';                   % transpose input
+targets = targets';                 % transpose target
+targetsd = targetsd';               % transpose dummy variable
+
+%% partitioning the dataset based on random selection of indices
+rng(1);                             % for reproducibility
+patitionObject = cvpartition(n,'Holdout',int8(n/3.0));   % hold out 1/3 of the dataset
+
+Xtrain = inputs(:, training(patitionObject));    % 2/3 of the input for training
+Ytrain = targetsd(:, training(patitionObject));  % 2/3 of the target for training
+
+Xtest = inputs(:, test(patitionObject));         % 1/3 of the input for testing
+Ytest = targets(test(patitionObject));           % 1/3 of the target for testing
+Ytestd = targetsd(:, test(patitionObject));      % 1/3 of the dummy variable for testing
+
+%% Sweep Code Block
+%Sweeping to choose different sizes for the hidden layer
+
+sweep = [10,50:50:1000];                 % parameter values to test
+scores = zeros(length(sweep), length(sweep));       % pre-allocation
+% we will use models to save the several neural network result from this
+% sweep and run loop
+models = cell(length(sweep), length(sweep));        % pre-allocation
+x = Xtrain;                             % inputs
+t = Ytrain;                             % targets
+trainFcn = 'trainscg';                  % scaled conjugate gradient
+for i = 1:length(sweep)
+
+    hiddenLayerSize = sweep(i);         % number of hidden layer neurons
+    net = patternnet(hiddenLayerSize);  % pattern recognition network
+    net.divideParam.trainRatio = 70/100;% 70% of data for training
+    net.divideParam.valRatio = 15/100;  % 15% of data for validation
+    net.divideParam.testRatio = 15/100; % 15% of data for testing
+    net = train(net, x, t);             % train the network
+    models{i} = net;                    % store the trained network
+    p = net(Xtest);                     % predictions
+    [~, p] = max(p);                    % predicted labels
+    scores(i) = sum(Ytest == p) /length(Ytest);  % categorization accuracy
+
+end
+% Let's now plot how the categorization accuracy changes versus number of 
+% neurons in the hidden layer.
+figure;
+plot(sweep, scores)
+xlabel('number of hidden neurons')
+ylabel('categorization accuracy')
+title('Number of hidden neurons vs. accuracy')
--- Project/main.m
+++ Project/main.m
@@ -1,22 +1,43 @@
-
 % Author(s): Aaron Barraclough, Michael Ferry, Steven Roote, Corey Zalewski
 
+detector = vision.ForegroundDetector('NumTrainingFrames', 200, 'AdaptLearningRate', 'false');
+videoPlayer = vision.VideoPlayer('Position',[100,100,680,520]);
+cam = webcam(3);
+
+%% Train GMM
+for i = 1 : 200
+    im = snapshot(cam);
+    frame = rgb2gray(im);    
+    frameFilter = filter2(fspecial('average',2),frame)/255;
+    step(detector, frameFilter);
+    pause(0.005);
+end
 %% Preprocessing (Image Processing and Feature Extraction)
+while ~isDone(videoPlayer)
 
-% Recieve New Frame From Webcam
-% INSERT CODE HERE
+    % Recieve New Frame From Webcam
+    im = snapshot(cam);
+    frame = rgb2gray(im);
 
-% Non-linear Filter for Noise Reduction
-frameFilter = filter2(fspecial('average',2),frame)/255;
-figure;
-imshow(frameFilter);
+    % Non-linear Filter for Noise Reduction
+    frameFilter = filter2(fspecial('average',2),frame)/255;
+%     figure;
+%     imshow(frameFilter);
 
-% Gaussian Mixture Model for clustering image
-frameGMM = gmdistribution(frameFilter, 'full');
-figure;
-imshow(frameGMM);
+    % Gaussian Mixture Model for clustering image
+    mask = step(detector, frameFilter);
+    frameGMM = mask .* frameFilter;
+%     figure;
+%     imshow(frameGMM);
 
-% Use Canny Edge Detection Algorithm to generate binary file with edges
-frameCannyEdge = edge(frameGMM,'Canny');
-figure;
-imshow(frameCannyEdge);
+    % Use Canny Edge Detection Algorithm to generate binary file with edges
+    frameCannyEdge = edge(frameGMM,'Canny');
+%     figure;
+%     imshow(frameCannyEdge);
+
+    % Show final
+    step(videoPlayer,frameCannyEdge);
+end
+release(videoPlayer);
+release(detector);
+clear cam;
