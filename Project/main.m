247e6e550476e873a61dac5954efa53a964726f9
Serenity62 <cuz126@psu.edu>
2018-04-25 22:35:22 -0400

    Working main
    pi cam still works half the time

--- Project/main.m
+++ Project/main.m
@@ -4,10 +4,11 @@ clear;
 close all;
 
 mypi = raspi('192.168.110.154','pi','raspberry');
-videoPlayer = vision.VideoPlayer('Position',[100,100,680,520]);
+% videoPlayer = vision.VideoPlayer('Position',[100,100,680,520]);
 cam = webcam(mypi,char(mypi.AvailableWebcams(1)), '640x480');
 % cam.Resolution = '640x480'; /dev/video0 or HD Webcam C615 (usb-3f980000.usb-1.3):
 load('nn.mat'); %bestNN is the name for the nn
+im = snapshot(cam);
 %% Preprocessing (Image Processing and Feature Extraction)
 while true
 
@@ -25,15 +26,17 @@ while true
     sz = size(frameCannyEdge); %size of CannyEdge frame
     fv = reshape(frameCannyEdge,[1,sz(1)*sz(2)]); 
     
-    % Feed feature vector to NN    
-    p = bestNN(datann2gpu(fv)); %prediction of NN(1-10)
-    p = gpu2datann(p);
+    % Feed feature vector to NN
+    fv = fv(1:239)';
+    p = bestNN(fv); %prediction of NN(1-10)
+    [~, p] = max(p);
+%     p = gpu2nndata(p);
     
     % Feed prediction to API caller
 %     API_caller(p);
     disp(p);
     % Show final
-    step(videoPlayer,frameCannyEdge);
+%     step(videoPlayer,frameCannyEdge);
 end
-release(videoPlayer);
+% release(videoPlayer);
 clear cam;
